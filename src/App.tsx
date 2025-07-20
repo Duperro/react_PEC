ec131836aad01f45683999deb689d65ea6678dd6
Rafael Alejandro Bermeo Macias <rafael.bermeo@cu.ucsg.edu.ec>
2025-07-20 14:32:10 -0500

    Segunda Tarea de PEC

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import Card, { CardBody } from "./components/Card";
 import LIst from "./components/LIst";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const list = ["Pepé", "Carlos", "Rafael", "Juan", "Pedro"];
+  const [data, setData] = useState([
+    "Pepe",
+    "Carlos",
+    "Rafael",
+    "Juan",
+    "Pedro",
+  ]);
+
+  /*
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
+  const list = ["Pepe", "Carlos", "Rafael", "Juan", "Pedro"];
   const handleSelect = (elemento: string) => {
     console.log("Elemento seleccionado:", elemento);
   };
+
   return (
     <Card>
       <CardBody
         title="Holamundo"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
       />
-      <LIst data={list} onSelect={handleSelect} />
+      {list.length !== 0 && <LIst data={list} onSelect={handleSelect} />}
+      <Button isLoading={isLoading} onClick={handleClick}>
+        Hola Mundo
+      </Button>
+    </Card>
+  );
+  */
+
+  const addMInion = () => setData([...data, "Carlos Arias"]);
+  const removeMInion = () => setData(data.slice(0, -1));
+
+  return (
+    <Card>
+      <Button onClick={addMInion}>Agregar</Button>
+      <Button onClick={removeMInion}>Eliminar</Button>
+      <LIst data={data}></LIst>
+      Hola Mundo
     </Card>
   );
 }
-
 export default App;
